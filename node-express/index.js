a0b285ce53d2d54f9e484929867e7e5969b470b8
Xiangyu Zhang <leonfigocr17@gmail.com>
2020-08-18 23:04:18 -0400

    Express rouer

--- node-express/index.js
+++ node-express/index.js
@@ -3,6 +3,7 @@ const express = require('express'),
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -12,28 +13,7 @@ app.use(bodyParser.json());
 
 
 
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
-
-app.get('/dishes', (req, res, next) => {
-    res.end('Will send all the dishes to you!');
-});
-
-app.post('/dishes', (req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.put('/dishes', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-});
-
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all dishes');
-});
+app.use('/dishes', dishRouter);
 
 app.get('/dishes/:dishId', (req, res, next) => {
     res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
