2c72d2a781efa19a55882f0e083afd67a8db26ec
Xiangyu Zhang <leonfigocr17@gmail.com>
2020-08-20 16:22:19 -0400

    Mongoose part3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -6,28 +6,45 @@ const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
+	console.log('Connected correctly to server');
+	Dishes.create({
+		name: 'Uthappizza',
+		description: 'test'
+	})
+		.then((dish) => {
+			console.log(dish);
 
-    console.log('Connected correctly to server');
+			return Dishes.findByIdAndUpdate(
+				dish._id,
+				{
+					$set: { description: 'Updated test' }
+				},
+				{
+                    //return unpdated dishes
+					new: true
+				}
+			).exec();
+		})
+		.then((dish) => {
+			console.log(dish);
 
-    Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
-    })
-        .then((dish) => {
-            console.log(dish);
+			dish.comments.push({
+				rating: 5,
+				comment: "I'm getting a sinking feeling!",
+				author: 'Leonardo di Carpaccio'
+			});
 
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log(dishes);
+			return dish.save();
+		})
+		.then((dish) => {
+			console.log(dish);
 
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
+			return Dishes.remove({});
+		})
+		.then(() => {
+			return mongoose.connection.close();
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
