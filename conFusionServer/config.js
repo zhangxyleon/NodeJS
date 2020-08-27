387c220b8700fc217adf08e3f7c39a84a1220504
Xiangyu Zhang <leonfigocr17@gmail.com>
2020-08-27 19:12:19 -0400

    passport facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
-}
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1155495738183107',
+        clientSecret: 'c86cc3db3e5d238ff9013d69fd163a94'
+    }
+} 
